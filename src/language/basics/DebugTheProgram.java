49a1bbc0fed70d6feff4b5d9dac9dfeee1987d88
chanduc591 <Sekharc5916@gmail.com>
2025-04-07 22:40:09 +0530

    Assignments

---
+++ src/language/basics/DebugTheProgram.java
@@ -0,0 +1,17 @@
+package language.basics;
+
+public class DebugTheProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Step1");
+		System.out.println("Step2");
+		System.out.println("Step3");
+		System.out.println("Step4");
+		System.out.println("Step5"+5/(4+7+3));
+		System.out.println("Step6");
+		System.out.println("Excution is completed");
+	}
+
+}
